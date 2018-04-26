18760ce18cde4bf36ac360d386a88833dd5b413e
NaoTaru <34298081+NaoTaru@users.noreply.github.com>
2018-04-26 08:42:02 +0000

    create template

---
+++ src/salvator-x_led/build/setting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x
+
+source /home/vagrant/share/yocto_xenial/yocto_sdk/2.4.1/environment-setup-aarch64-poky-linux
+export LDFLAGS=""
+export SYSROOT_PREFIX=/home/vagrant/share/yocto_xenial/yocto_sdk/2.4.1/sysroots/aarch64-poky-linux
+
