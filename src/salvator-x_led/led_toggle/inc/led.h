2059f91a3c12bdee59a1e688c127f05e510bdf5c
NaoTaru <34298081+NaoTaru@users.noreply.github.com>
2018-07-30 12:19:36 +0000

    update main program

---
+++ src/salvator-x_led/led_toggle/inc/led.h
@@ -0,0 +1,7 @@
+#define	LED4_DEVICE	"/dev/led4"
+
+#define RET_OK	(0)
+#define RET_NG	(1)
+
+#define	LED_OFF	(0)
+#define	LED_ON	(1)
--- src/salvator-x_led/led_toggle/src/led.c
+++ src/salvator-x_led/led_toggle/src/led.c
@@ -1,7 +1,61 @@
-#include<stdio.h>
-#include<linux/gpio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>	/* O_RDWR */
+#include <unistd.h>	/* write close sleep */
 
-int main(){
+#include <led.h>
+
+int led_ctrl( int val )
+{
+	int fd;
+	char c;
+
+	fd = open( LED4_DEVICE,  O_RDWR );
+
+	if( fd < 0 )
+	{
+		perror("LED4_DEVICE device file open failed");
+		exit(RET_NG);
+	}
+
+	if( val == 0 )
+	{
+		c = '0';
+	}
+	else
+	{
+		c = '1';
+	}
+
+	write( fd, &c, 1 );
+
+	close( fd );
+
+	return RET_OK;
+}
+
+int main()
+{
+	int i;
+
     printf("Hello,World!!\n");
-    return 0;
+
+	/* Toggle Output Level */
+	for( i = 0 ; i < 1/*min*/ * 60/*sec*/ ; i++ )
+	{
+		sleep(1);
+
+		if( i % 2 == 0 )
+		{
+			led_ctrl( LED_OFF );
+		}
+		else
+		{
+			led_ctrl( LED_ON );
+		}
+	}
+
+    printf("Finish!\n");
+
+    return RET_OK;
 }
